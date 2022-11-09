6d027c3d30d7e7a69e39db18292e67cc9147315b
PedroAugustoB <99665373+PedroAugustoB@users.noreply.github.com>
2022-11-09 08:33:05 -0300

    Add files via upload

---
+++ dia a dia.py
@@ -0,0 +1,31 @@
+
+print("dias da semana")
+nome = int(input("Informe o dia da semana emtre 1 a 7:"))
+
+
+print("========================================")
+
+if(nome == 1):
+  print("Domingo")
+  print("vc esta de ferias")
+elif (nome == 2):
+  print("Segunda")
+  print("vc n esta de ferias")
+elif (nome == 3):
+  print("terça")
+  print("vc n esta de ferias")
+elif (nome == 4):
+  print("quarta")
+  print("vc n esta de ferias")
+elif (nome == 5):
+  print("quinta")
+  print("vc n esta de ferias")
+elif (nome == 6):
+  print("sexta")
+  print("vc n esta de ferias")
+elif (nome == 7):
+  print("sabado")
+  print("vc esta de ferias")
+else:
+  print("esse numeto n é permitido ")
+print("Processo finalizado!")
